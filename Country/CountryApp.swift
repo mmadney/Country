9f9570321102a342405028f148a2efcca4096b7b
madney <135223616+Engmadney@users.noreply.github.com>
2025-11-14 19:00:14 +0200

    Initial Commit

---
+++ Country/CountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryApp.swift
+//  Country
+//
+//  Created by Madney on 14/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
